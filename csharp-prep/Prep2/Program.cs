e19066cd81036fd0e317c1fd5b80e26ff031e940
Neunhoffer <jefferybob12345@gmail.com>
2026-04-25 20:31:08 -0600

    completed preps 1 & 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Hello please type your grade percentage in here: ");
+        string letter = "";
+        string grade = Console.ReadLine();
+        float number = float.Parse(grade);
+        if (number >= 70.0)
+        {
+            Console.WriteLine("Congratulations you passed the class!!!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you failed the class:( Retake it next year and you will pass for sure!!!");
+        }
+        if (number >= 90.0)
+        {
+            letter = "A";
+        }
+        else if (number >= 80.0)
+        {
+            letter = "B";
+        }
+        else if (number >= 70.0)
+        {
+            letter = "C";
+        }
+        else if (number >= 60.0)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"You completed the class with {letter} as your final grade.");
     }
+    
 }
