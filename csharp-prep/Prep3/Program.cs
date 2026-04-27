c2f1ccc7aff3b84f30a8486c1464d9c8ca608fe9
Neunhoffer <jefferybob12345@gmail.com>
2026-04-27 17:23:28 -0600

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random randomGenerator = new Random();
+        int magicnumber = randomGenerator.Next(1,20);
+
+        int guess = 0;
+
+        while (guess != magicnumber)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+            if (guess > magicnumber)
+            {
+                Console.WriteLine("Nice try but your number is a little bit too high");
+            }
+            else if (guess < magicnumber)
+            {
+                Console.WriteLine("Nice try but your number is a little bit too low");
+            }
+            else if (guess == magicnumber)
+            {
+                Console.WriteLine("CONGRATULATIONS you guessed the number");
+            }
+        }
     }
 }
