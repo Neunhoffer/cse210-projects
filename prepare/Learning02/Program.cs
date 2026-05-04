58903f8190715f0d2f521031c8384155ca9be409
Neunhoffer <jefferybob12345@gmail.com>
2026-05-04 17:07:21 -0600

    learning 2 classes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._name = "Allison Rose";
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._name = "Larry Wheels";
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2020;
+        job2._endYear = 2026;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Allison Rose";
+        
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
