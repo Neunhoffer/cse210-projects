7cd05926c5c2ca3b33c19d0a7ae8fdb03e5a5b97
Neunhoffer <jefferybob12345@gmail.com>
2026-05-18 17:17:06 -0600

    Learning 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f = new Fraction();
+        Random random = new Random();
+
+        for (int i = 0; i < 20; i++)
+        {
+            int top = random.Next(1, 20);
+            int bottom = random.Next(1, 20);
+            f.SetTop(top);
+            f.SetBottom(bottom);
+            Console.WriteLine($"Fraction {i + 1}: string: {f.GetFractionString()} Number: {f.GetDecimalValue()}");
+        }
+
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(5, 10);
+
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+        Console.WriteLine(f1.GetBottom());
+        Console.WriteLine(f1.GetTop());
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+        Console.WriteLine(f2.GetBottom());
+        Console.WriteLine(f2.GetTop());
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+        Console.WriteLine(f3.GetBottom());
+        Console.WriteLine(f3.GetTop());
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+        Console.WriteLine(f3.GetBottom());
+        Console.WriteLine(f3.GetTop());
     }
 }
