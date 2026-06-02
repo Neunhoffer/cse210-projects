a776b60a5e0bc1128b34c974f6defb8cac4df270
Neunhoffer <jefferybob12345@gmail.com>
2026-06-01 22:40:44 -0600

    Inheritance practice

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Bob", "History");
+        Console.WriteLine(assignment1.GetSummary());
+        
+        MathAssignment mathassignment1 = new MathAssignment("Doug", "Math", "Section 8.4", "Problems 10-20");
+        Console.WriteLine(mathassignment1.GetHomeworkList());
+
+        WritingAssignment writingassignment1 = new WritingAssignment("Fred","English" ,"The Adventures of Peabody");
+        Console.WriteLine(writingassignment1.GetWritingInformation());
     }
 }
